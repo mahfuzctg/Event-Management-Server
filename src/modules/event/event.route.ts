2939d9b2a6d7413d2c29e308c766b253c20e479e
Mahfuz <aamahfuz.pro@gmail.com>
2025-08-16 00:46:06 +0600

    modules: event-route added

---
+++ src/modules/event/event.route.ts
@@ -0,0 +1,22 @@
+import { Router } from "express";
+import {
+  createEvent,
+  getAllEvents,
+  getEventById,
+  updateEvent,
+  deleteEvent,
+} from "./event.controller";
+import { authMiddleware} from "../../middlewares/auth.middleware"; 
+
+const router = Router();
+
+// Public routes
+router.get("/", getAllEvents);
+router.get("/:id", getEventById);
+
+// Admin routes (protected)
+router.post("/", authMiddleware, createEvent);
+router.put("/:id", authMiddleware, updateEvent);
+router.delete("/:id", authMiddleware, deleteEvent);
+
+export default router;
