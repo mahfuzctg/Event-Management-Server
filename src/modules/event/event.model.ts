4ab90b43285dfed8627ae0f0b872115bc4fca2a4
Mahfuz <aamahfuz.pro@gmail.com>
2025-08-15 22:46:46 +0600

    modules: event-models added

---
+++ src/modules/event/event.model.ts
@@ -0,0 +1,26 @@
+import { Schema, model } from "mongoose";
+import { IEventDoc, EventStatus } from "./event.interface";
+import dayjs from "dayjs";
+
+const eventSchema = new Schema<IEventDoc>({
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  date: { type: Date, required: true },
+  endDate: { type: Date }, 
+  location: { type: String },
+  image: { type: String },
+  status: { type: String, enum: ["Upcoming", "Ongoing", "Past"], default: "Upcoming" },
+}, { timestamps: true });
+
+// Pre-save hook to calculate status automatically
+eventSchema.pre("save", function (next) {
+  const now = dayjs();
+  const start = dayjs(this.date);
+  const end = this.endDate ? dayjs(this.endDate) : start.add(2, "hour"); // default 2-hour event
+  if (now.isBefore(start)) this.status = "Upcoming";
+  else if (now.isAfter(end)) this.status = "Past";
+  else this.status = "Ongoing";
+  next();
+});
+
+export const EventModel = model<IEventDoc>("Event", eventSchema);
